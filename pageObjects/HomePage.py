ec48a6dd0a8f06f048534501653eed8155aaae40
Yashas <yashasbharadwaj6@gmail.com>
2024-07-18 18:52:53 +0530

    confirming addition of products to cart

--- pageObjects/HomePage.py
+++ pageObjects/HomePage.py
@@ -22,6 +22,12 @@ class HomePage(BaseClass):
     laptopsCategoryLink = (By.LINK_TEXT, "Laptops")
     monitorsCategoryLink = (By.LINK_TEXT, "Monitors")
 
+    nextButton = (By.CSS_SELECTOR, "#next2")
+    lastproductLink = (By.LINK_TEXT, "MacBook Pro")
+    lastproductName = (By.CSS_SELECTOR, ".name")
+    lastproductPrice = (By.CSS_SELECTOR, ".price-container")
+    addToCartButton = (By.LINK_TEXT, "Add to cart")
+
     def __init__(self, driver):
         self.driver = driver
 
@@ -63,28 +69,28 @@ class HomePage(BaseClass):
         loginText = self.driver.find_element(*HomePage.loginLink).text
         return loginText
 
-    def performProductBrowsingCategoryWise(self, productsData,log):
+    def performProductBrowsingCategoryWise(self, productsData, log):
         category = productsData["category"]
         products = productsData["products"]
         actualProducts = {}
         if category == "phones":
             self.driver.find_element(*HomePage.phonesCategoryLink).click()
-            _list = self.verifyActualProductsPresenceAndPopulate(products,log)
+            _list = self.verifyActualProductsPresenceAndPopulate(products, log)
             actualProducts["category"] = "phones"
             actualProducts["products"] = _list
         elif category == "laptops":
             self.driver.find_element(*HomePage.laptopsCategoryLink).click()
-            _list = self.verifyActualProductsPresenceAndPopulate(products,log)
+            _list = self.verifyActualProductsPresenceAndPopulate(products, log)
             actualProducts["category"] = "laptops"
             actualProducts["products"] = _list
         elif category == "monitors":
             self.driver.find_element(*HomePage.monitorsCategoryLink).click()
-            _list = self.verifyActualProductsPresenceAndPopulate(products,log)
+            _list = self.verifyActualProductsPresenceAndPopulate(products, log)
             actualProducts["category"] = "monitors"
             actualProducts["products"] = _list
         return actualProducts
 
-    def verifyActualProductsPresenceAndPopulate(self, products,log):
+    def verifyActualProductsPresenceAndPopulate(self, products, log):
         li = []
         for product in products:
             try:
@@ -94,3 +100,17 @@ class HomePage(BaseClass):
                 log.error(f"Product link text '{product}' not found.")
                 li.append(None)
         return li
+
+    def performAddToCart(self):
+        self.driver.find_element(*HomePage.nextButton).click()
+        self.verifyLinkPresence("MacBook Pro")
+        self.driver.find_element(*HomePage.lastproductLink).click()
+        productName = self.driver.find_element(*HomePage.lastproductPrice).text
+        productPrice = self.driver.find_element(*HomePage.lastproductPrice).text
+        self.driver.find_element(*HomePage.addToCartButton).click()
+        return productName, productPrice
+
+    def confirmAddToCart(self):
+        self.performAddToCart()
+        actualAlertText = self.returnAlertText()
+        return actualAlertText
